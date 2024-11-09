79d45973bef5edf4ea485ec09d880bdd5d6c53cf
Gracfusie <109655820+Gracfusie@users.noreply.github.com>
2024-11-09 14:10:34 +0800

    1109-4 checker transmute

---
+++ transmute/main.cpp
@@ -0,0 +1,32 @@
+#include "global.h"
+#include "object.h"
+#include "lib.h"
+#include "arch.h"
+#include "netlist.h"
+
+int main(int, char* argv[])
+{
+    std::string nodeFileName = argv[1];
+    std::string netFileName = argv[2];
+    std::string timingFileName = argv[3];
+    std::string outputFileName = argv[4]; 
+
+    std::string libFileName = "../benchmark/fpga.lib";
+    // on server: "/home/public/Arch/fpga.lib"
+    std::string sclFileName = "../benchmark/fpga.scl";
+    // on server: "/home/public/Arch/fpga.scl"
+    std::string clkFileName = "../benchmark/fpga.clk";
+    // on server: "/home/public/Arch/fpga.clk"
+
+    readAndCreateLib(libFileName);  // from lib.cpp
+    chip.readArch(sclFileName, clkFileName);
+    chip.reportArch();
+    readInputNodes(nodeFileName);
+    readInputNets(netFileName);
+    readInputTiming(timingFileName);
+    std::cout << "  Successfully read design files." << std::endl;
+    reportDesignStatistics();   // from netlist.cpp
+    std::cout << "the output file name is " << outputFileName << std::endl;
+
+    return 0;
+}
