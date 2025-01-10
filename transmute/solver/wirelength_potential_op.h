0d3c9f31d37131741df695efd6c8a1ee5399b50f
Worldline22 <qinyuchao@stu.pku.edu.cn>
2025-01-10 12:52:35 +0800

    cache for wirelength

---
+++ transmute/solver/wirelength_potential_op.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "solverObject.h"
+#include "checker_legacy/rsmt.h"
+#include <cassert>
+
+extern RecSteinerMinTree rsmt;
+
+// void getMergedNonCritPinLocs(const SNet& net, std::vector<int>& xCoords, std::vector<int>& yCoords);
+
+// int calculateWirelength(const SNet& net);
+
+// int calculateInstanceWirelength(const SInstance& instance);
+
+// int calculateWirelengthIncrease(const SInstance& instance_old, std::tuple<int, int, int> newLocation);
+
+int calculate_WL_Increase(SInstance* inst_old_ptr, std::tuple<int, int, int> newLoc);
